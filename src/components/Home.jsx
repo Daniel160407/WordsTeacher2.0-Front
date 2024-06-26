1c325c639114b2971c26061b2cc9771e4cd08f42
Daniel160407 <abulashvilidani@hotmail.com>
2024-06-26 15:09:26 +0400

    Added ability to send words

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import '../style/Home.scss';
 
 // eslint-disable-next-line react/prop-types
-const Home = ({updatedWords}) => {
+const Home = ({ updatedWords }) => {
     const [words, setWords] = useState([]);
+    const checkboxesRef = useRef({});
 
     useEffect(() => {
         axios.get('http://localhost:8080/wordsTeacher/words')
@@ -14,17 +15,33 @@ const Home = ({updatedWords}) => {
     }, []);
 
     useEffect(() => {
-        if(updatedWords !== ''){
+        if (updatedWords !== '') {
             setWords(updatedWords);
         }
-    }, [updatedWords])
+    }, [updatedWords]);
+
+    const sendWords = () => {
+        const checkedWords = words.filter(word => checkboxesRef.current[word.word]?.checked);
+        axios.put('http://localhost:8080/wordsTeacher/dropper', checkedWords)
+            .then(response => {
+                setWords(response.data);
+            });
+    };
 
     return (
         <div id="words" className="tab-pane fade show active">
             {words.map(word => (
-                <div className="word" key={word.word + word.meaning}>{word.word} - {word.meaning}</div>
+                <div className="word" key={word.word + word.meaning}>
+                    <h1>{word.word} - {word.meaning}</h1>
+                    <input 
+                        type="checkbox"
+                        ref={el => checkboxesRef.current[word.word] = el}
+                    />
+                </div>
             ))}
+            <button type="button" className="btn btn-warning" onClick={sendWords}>Send</button>
         </div>
     );
 }
+
 export default Home;
