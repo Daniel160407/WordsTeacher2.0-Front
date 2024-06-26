5e1de40192b56b42845fd3959369197dd6f0e046
Daniel160407 <abulashvilidani@hotmail.com>
2024-06-26 18:44:51 +0400

    Added level management

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,12 +5,18 @@ import '../style/Home.scss';
 // eslint-disable-next-line react/prop-types
 const Home = ({ updatedWords }) => {
     const [words, setWords] = useState([]);
+    const [level, setLevel] = useState(0);
     const checkboxesRef = useRef({});
 
     useEffect(() => {
         axios.get('http://localhost:8080/wordsTeacher/words')
             .then(response => {
                 setWords(response.data);
+            });
+
+        axios.get('http://localhost:8080/wordsTeacher/words/level')
+            .then(response => {
+                setLevel(response.data.level);
             });
     }, []);
 
@@ -25,11 +31,16 @@ const Home = ({ updatedWords }) => {
         axios.put('http://localhost:8080/wordsTeacher/dropper', checkedWords)
             .then(response => {
                 setWords(response.data);
+                axios.get('http://localhost:8080/wordsTeacher/words/level')
+                    .then(response => {
+                    setLevel(response.data.level);
+                });
             });
     };
 
     return (
         <div id="words" className="tab-pane fade show active">
+            <h1>Level {level}</h1>
             {words.map(word => (
                 <div className="word" key={word.word + word.meaning}>
                     <h1>{word.word} - {word.meaning}</h1>
