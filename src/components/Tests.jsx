f8ed3d376be46aa8a72dadd686680cc7df7a7433
Daniel160407 <abulashvilidani@hotmail.com>
2024-06-27 17:14:03 +0400

    After clicking the button, correctly spelled words are displayed

--- src/components/Tests.jsx
+++ src/components/Tests.jsx
@@ -1,39 +1,83 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import '../style/Tests.scss';
 
 const Tests = () => {
     const [language, setLanguage] = useState('GEO');
+    const [wordsList, setWordsList] = useState('words');
     const [words, setWords] = useState([]);
     const [meanings, setMeanings] = useState([]);
     const [inputs, setInputs] = useState({});
     const [overallFeedback, setOverallFeedback] = useState({});
 
     useEffect(() => {
-        axios.get('http://localhost:8080/wordsTeacher/words')
-            .then(response => {
-                const data = response.data;
-                if (language === 'GEO') {
-                    setWords(data.map(item => item.word));
-                    setMeanings(data.map(item => item.meaning));
-                } else {
-                    setWords(data.map(item => item.meaning));
-                    setMeanings(data.map(item => item.word));
-                }
+        if (wordsList === 'words') {
+            axios.get('http://localhost:8080/wordsTeacher/words')
+                .then(response => {
+                    const data = response.data;
+                    if (language === 'GEO') {
+                        setWords(data.map(item => item.word));
+                        setMeanings(data.map(item => item.meaning));
+                    } else {
+                        setWords(data.map(item => item.meaning));
+                        setMeanings(data.map(item => item.word));
+                    }
 
-                setInputs({});
-                setOverallFeedback({});
-            });
-    }, [language]);
+                    setInputs({});
+                    setOverallFeedback({});
+                });
+        } else if (wordsList === 'droppedWords') {
+            axios.get('http://localhost:8080/wordsTeacher/dropper')
+                .then(response => {
+                    const data = response.data;
+                    if (language === 'GEO') {
+                        setWords(data.map(item => item.word));
+                        setMeanings(data.map(item => item.meaning));
+                    } else {
+                        setWords(data.map(item => item.meaning));
+                        setMeanings(data.map(item => item.word));
+                    }
+
+                    setInputs({});
+                    setOverallFeedback({});
+                });
+        } else if (wordsList === 'all') {
+            axios.get('http://localhost:8080/wordsTeacher/words')
+                .then(response1 => {
+                    let data1 = response1.data;
+                    axios.get('http://localhost:8080/wordsTeacher/dropper')
+                        .then(response2 => {
+                            let data2 = response2.data;
+                            let combinedData = [...data1, ...data2];
+
+                            if (language === 'GEO') {
+                                setWords(combinedData.map(item => item.word));
+                                setMeanings(combinedData.map(item => item.meaning));
+                            } else {
+                                setWords(combinedData.map(item => item.meaning));
+                                setMeanings(combinedData.map(item => item.word));
+                            }
+
+                            setInputs({});
+                            setOverallFeedback({});
+                        });
+                });
+
+        }
+    }, [language, wordsList]);
 
     const handleInputChange = (e, index) => {
-        const { value } = e.target;
-        setInputs(prevInputs => ({ ...prevInputs, [index]: value }));
+        const {value} = e.target;
+        setInputs(prevInputs => ({...prevInputs, [index]: value}));
     };
 
     const handleChangeLanguage = (e) => {
         setLanguage(e.target.value);
     };
+
+    const handleChangeWords = (e) => {
+        setWordsList(e.target.value);
+    }
 
     const checkAnswers = () => {
         const feedback = {};
@@ -41,7 +85,7 @@ const Tests = () => {
             if (inputs[index] && inputs[index].toLowerCase() === meanings[index].toLowerCase()) {
                 feedback[index] = '✅';
             } else {
-                feedback[index] = '❌';
+                feedback[index] = `❌ \n Correct: ${meanings[index]}`;
             }
         });
         setOverallFeedback(feedback);
@@ -53,6 +97,11 @@ const Tests = () => {
                 <select onChange={handleChangeLanguage}>
                     <option value="GEO">GEO</option>
                     <option value="DEU">DEU</option>
+                </select>
+                <select onChange={handleChangeWords}>
+                    <option value="words">Words</option>
+                    <option value="droppedWords">Dropped Words</option>
+                    <option value="all">All</option>
                 </select>
                 {words.map((word, index) => (
                     <div className="word" key={word}>
