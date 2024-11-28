65571be1ccc14bb89c017b8adf9b1b3cfa5e3197
Daniel160407 <abulashvilidani@hotmail.com>
2024-11-28 17:21:14 +0400

    Fixed bug with showing edit functions after long press on the word on Iphones

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 import { useEffect, useState, useRef } from "react";
-import '../style/Home.scss';
+import "../style/Home.scss";
 
-// eslint-disable-next-line react/prop-types
 const Home = ({ updatedWords, setUpdatedWords, setUpdatedDictionaryWords }) => {
     const [words, setWords] = useState([]);
     const [level, setLevel] = useState(0);
@@ -12,43 +11,36 @@ const Home = ({ updatedWords, setUpdatedWords, setUpdatedDictionaryWords }) => {
     const [editValue, setEditValue] = useState("");
     const [meaningEditValue, setMeaningEditValue] = useState("");
     const [wordTypeEditValue, setWordTypeEditValue] = useState("");
-    const [wordsType, setWordsType] = useState('word');
+    const [wordsType, setWordsType] = useState("word");
 
     useEffect(() => {
         axios.get(`http://localhost:8080/wordsTeacher/words?wordstype=${wordsType}`)
-            .then(response => {
-                setWords(response.data);
-            });
+            .then(response => setWords(response.data));
 
-        axios.get('http://localhost:8080/wordsTeacher/words/level')
-            .then(response => {
-                setLevel(response.data.level);
-            });
+        axios.get("http://localhost:8080/wordsTeacher/words/level")
+            .then(response => setLevel(response.data.level));
     }, [wordsType]);
 
     useEffect(() => {
-        if (updatedWords !== '') {
+        if (updatedWords !== "") {
             setWords(updatedWords);
         }
     }, [updatedWords]);
 
     const sendWords = () => {
         const checkedWords = words.filter(word => checkboxesRef.current[word.word]?.checked);
-        axios.put('http://localhost:8080/wordsTeacher/dropper', checkedWords)
+        axios.put("http://localhost:8080/wordsTeacher/dropper", checkedWords)
             .then(response => {
                 setWords(response.data);
-                axios.get('http://localhost:8080/wordsTeacher/words/level')
-                    .then(response => {
-                    setLevel(response.data.level);
-                });
+                axios.get("http://localhost:8080/wordsTeacher/words/level")
+                    .then(response => setLevel(response.data.level));
             });
-            Object.values(checkboxesRef.current).forEach(checkbox => {
-                if (checkbox) checkbox.checked = false;
-            });
+        Object.values(checkboxesRef.current).forEach(checkbox => {
+            if (checkbox) checkbox.checked = false;
+        });
     };
 
-    const handleContextMenu = (e, word) => {
-        e.preventDefault();
+    const handleContextMenu = (word) => {
         setVisibleWord(word.word === visibleWord ? null : word.word);
     };
 
@@ -59,26 +51,21 @@ const Home = ({ updatedWords, setUpdatedWords, setUpdatedDictionaryWords }) => {
         setWordTypeEditValue(word.wordType);
     };
 
-    const handleEditChange = (e) => {
-        setEditValue(e.target.value);
-    };
-
-    const handleMeaningEditChange = (e) => {
-        setMeaningEditValue(e.target.value);
-    }
+    const handleEditChange = (e) => setEditValue(e.target.value);
+    const handleMeaningEditChange = (e) => setMeaningEditValue(e.target.value);
 
     const handleKeyPress = (e, word) => {
-        if(e.target.value === 'Enter'){
+        if (e.key === "Enter") {
             handleSave(word);
         }
-    }
+    };
 
     const handleSave = (word) => {
         const changedWord = {
             word: editValue,
             meaning: meaningEditValue,
             wordType: wordTypeEditValue
-        }
+        };
 
         const changedWordArray = [word, changedWord];
         axios.put(`http://localhost:8080/wordsTeacher/words`, changedWordArray)
@@ -88,10 +75,8 @@ const Home = ({ updatedWords, setUpdatedWords, setUpdatedDictionaryWords }) => {
                 setEditWord(null);
                 setEditValue("");
 
-                axios.put('http://localhost:8080/wordsTeacher/dictionary', changedWordArray)
-                    .then(response => {
-                        setUpdatedDictionaryWords(response.data);
-                    });
+                axios.put("http://localhost:8080/wordsTeacher/dictionary", changedWordArray)
+                    .then(response => setUpdatedDictionaryWords(response.data));
             });
     };
 
@@ -102,47 +87,63 @@ const Home = ({ updatedWords, setUpdatedWords, setUpdatedDictionaryWords }) => {
                 setUpdatedWords(response.data);
 
                 axios.delete(`http://localhost:8080/wordsTeacher/dictionary?word=${word.word}&meaning=${word.meaning}`)
-                    .then(response => {
-                        setUpdatedDictionaryWords(response.data);
-                    });
+                    .then(response => setUpdatedDictionaryWords(response.data));
             });
+    };
+
+    const handleTouchStart = (e, word) => {
+        e.persist();
+        const touchTimeout = setTimeout(() => {
+            handleContextMenu(word);
+        }, 500);
+
+        e.target.touchTimeout = touchTimeout;
+    };
+
+    const handleTouchEnd = (e) => {
+        clearTimeout(e.target.touchTimeout);
     };
 
     return (
         <div id="words" className="tab-pane fade show active">
             <h1>Level {level}</h1>
             <select onChange={(e) => setWordsType(e.target.value)}>
-                <option value={'word'}>Words</option>
-                <option value={'difficult'}>Difficult Verbs</option>
+                <option value="word">Words</option>
+                <option value="difficult">Difficult Verbs</option>
             </select>
             {words.map(word => (
-                <div 
-                    className="word" 
-                    key={word.word + word.meaning} 
-                    onContextMenu={(e) => handleContextMenu(e, word)}
+                <div
+                    className="word"
+                    key={word.word + word.meaning}
+                    onContextMenu={(e) => {
+                        e.preventDefault();
+                        handleContextMenu(word);
+                    }}
+                    onTouchStart={(e) => handleTouchStart(e, word)}
+                    onTouchEnd={handleTouchEnd}
                 >
                     {editWord === word.word ? (
                         <div className="editWordContainer">
-                            <input 
-                                type="text" 
-                                className="edit-input" 
-                                value={editValue} 
-                                onChange={handleEditChange} 
+                            <input
+                                type="text"
+                                className="edit-input"
+                                value={editValue}
+                                onChange={handleEditChange}
                                 onKeyPress={(e) => handleKeyPress(e, word)}
                             />
-                            <input 
-                                type="text" 
-                                className="edit-input" 
-                                value={meaningEditValue} 
-                                onChange={handleMeaningEditChange} 
+                            <input
+                                type="text"
+                                className="edit-input"
+                                value={meaningEditValue}
+                                onChange={handleMeaningEditChange}
                                 onKeyPress={(e) => handleKeyPress(e, word)}
                             />
                             <select className="editSelect" onChange={(e) => setWordTypeEditValue(e.target.value)}>
-                                <option value={'word'}>Words</option>
-                                <option value={'difficult'}>Difficult Verbs</option>
+                                <option value="word">Words</option>
+                                <option value="difficult">Difficult Verbs</option>
                             </select>
-                            <button 
-                                className="save-button btn btn-success" 
+                            <button
+                                className="save-button btn btn-success"
                                 onClick={() => handleSave(word)}
                             >
                                 Save
@@ -153,20 +154,32 @@ const Home = ({ updatedWords, setUpdatedWords, setUpdatedDictionaryWords }) => {
                     )}
                     {visibleWord === word.word && (
                         <div className="editWord">
-                            <img className="edit" src="/svg/pencil.svg" alt="edit" onClick={() => handleEdit(word)} />
-                            <img className="remove" src="/svg/trash.svg" alt="remove" onClick={() => handleRemove(word)} />
+                            <img
+                                className="edit"
+                                src="/svg/pencil.svg"
+                                alt="edit"
+                                onClick={() => handleEdit(word)}
+                            />
+                            <img
+                                className="remove"
+                                src="/svg/trash.svg"
+                                alt="remove"
+                                onClick={() => handleRemove(word)}
+                            />
                         </div>
                     )}
-                    <input 
+                    <input
                         className="checkbox"
                         type="checkbox"
-                        ref={el => checkboxesRef.current[word.word] = el}
+                        ref={el => (checkboxesRef.current[word.word] = el)}
                     />
                 </div>
             ))}
-            <button id="send-button" type="button" className="btn btn-warning" onClick={sendWords}>Drop</button>
+            <button id="send-button" type="button" className="btn btn-warning" onClick={sendWords}>
+                Drop
+            </button>
         </div>
     );
-}
+};
 
 export default Home;
